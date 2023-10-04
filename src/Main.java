108da852ca5eef4dd8ffdcbf8042da549f792b97
szak2137 <szak97495@gmail.com>
2023-10-04 09:12:08 +0200

    Testowanie

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(6+63);
+        int a = 12;
+        int b = 45;
+        System.out.println(a + b + " wynik");
+        System.out.println("wynik " + a + b);
+        System.out.println(b/a);
+        System.out.println( (double) b/a);
+    }
+}
